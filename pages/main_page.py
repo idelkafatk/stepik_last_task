1ae598c2b0e4cc0256a76295df4396bbdea615bd
idelkafatk <maximbravin@gmail.com>
2022-06-10 21:05:07 +0500

    добавил setup

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
 
 class MainPage(BasePage): 
     
--- test_product_page.py
+++ test_product_page.py
@@ -2,22 +2,35 @@ from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 import time
 
-#def test_guest_should_see_login_link_on_product_page(browser):
-    #link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    #page = ProductPage(browser, link)
-    #page.open()
-    #page.should_be_login_link()
+@pytest.mark.login
+class TestLoginFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.product = ProductFactory(title = "Best book created by robot")
+        # создаем по апи
+        self.link = self.product.link
+        yield
+        # после этого ключевого слова начинается teardown
+        # выполнится после каждого теста в классе
+        # удаляем те данные, которые мы создали 
+        self.product.delete()
 
-#def test_guest_can_go_to_login_page_from_product_page(browser):
-    #link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    #page = ProductPage(browser, link)
-    #page.open()
-    #page.go_to_login_page()
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+  
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
-    page = BasketPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    page.there_are_no_items()
-    page.is_message_about_no_items()
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+        page = BasketPage(browser, link)
+        page.open()
+        page.go_to_basket_page()
+        page.there_are_no_items()
+        page.is_message_about_no_items()
